32270f1accc4a50a798c6c4aa65ebad591ec84c7
rasslabsya <artyomlordik@gmail.com>
2025-04-18 18:56:57 +0300

    + tasks

--- task01/main.cpp
+++ task01/main.cpp
@@ -11,11 +11,17 @@ int main() {
 	init(matrix, size, 0, 100);
 	cout << "matrix:\n" << convert(matrix, size);
 	
-	cout << "sum of square matrix: " <<
+	cout << "sum matrix elements above main diagonal: " <<
 	sum_elements_up_main_diagonale(matrix, size) << endl;
 
-	cout << "sum of square matrix: " <<
+	cout << "sum matrix elements below main diagonal: " <<
 	sum_elements_down_main_diagonale(matrix, size) << endl;
+
+	cout << "sum matrix elements above additional diagonal:  " <<
+		sum_elements_up_additional_diagonale( matrix, size) << endl;
+
+	cout << "sum matrix elements below additional diagonal:  " <<
+		sum_elements_down_additional_diagonale(matrix, size) << endl;
 
 
 	system("pause");
