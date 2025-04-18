fa5737d98a13655b8043659a0662440ff39309a7
rasslabsya <artyomlordik@gmail.com>
2025-04-18 18:24:09 +0300

    Add project files.

---
+++ task01/util.h
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+#include <string>
+#define SIZE 100
+
+string convert(int matrix[SIZE][SIZE],int size);
+void init(int matrix[SIZE][SIZE],int size, int a,int b);
